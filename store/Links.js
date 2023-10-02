b043a95548d51594fcf9518dbd05824ff9ceb1c2
kylefb1050 <kyle.bandong01@gmail.com>
2023-10-01 21:34:30 -0400

    week6hw

---
+++ store/Links.js
@@ -0,0 +1,7 @@
+export default [
+  { title: "About", text: "About" },
+  { title: "Home", text: "Home" },
+  { title: "Links", text: "Links" },
+  { title: "List", text: "List" },
+  { title: "Model", text: "Models" }
+];
---
+++ store/List.js
@@ -0,0 +1,4 @@
+export default {
+  header: "Parts List",
+  view: "List"
+};
