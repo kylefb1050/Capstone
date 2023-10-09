0d1c7f7e79c49c3c7131c1c6624d530cabd08820
kylefb1050 <kyle.bandong01@gmail.com>
2023-10-09 17:36:53 -0400

    10/09 change

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,6 +3,11 @@ import html from "html-literal";
 export default state => html`
   <section id="jumbotron">
     <h1>Welcome to IntelliAuto!</h1>
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
     <p>
       Use this application to understand and develop self knowledge about your
       vehicle.
