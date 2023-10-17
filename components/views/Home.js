5a1ab4ecb08f4a7d75e9819647e5aad3d10b5c9e
kylefb1050 <kyle.bandong01@gmail.com>
2023-10-16 21:55:26 -0400

    week8 changes

--- components/views/Home.js
+++ components/views/Home.js
@@ -11,16 +11,10 @@ export default state => html`
             ${state.weather.description}. Temperature is ${state.weather.temp}F,
             and it feels like ${state.weather.feelsLike}F.
           </h3>
-          <p>
-            Use this application to understand and develop self knowledge about
-            vehicle... or fix it.
-          </p>
-          <img
-          src=https://files.porsche.com/filestore/image/multimedia/none/992-s-t-modelexplorer/normal/3b95cec4-217d-11ee-8103-005056bbdc38;sS;twebp065/porsche-normal.webp>
         </section>
 
         <div id="home-content">
-          <h2>What's wrong with you car?</h2>
+          <h2 id="h2-1">What's wrong with you car?</h2>
           <form id="form">
             <input type="search" id="query" name="q" placeholder="Search..." />
             <button>Search</button>
